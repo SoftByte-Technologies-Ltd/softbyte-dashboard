1bdeec81985f096997bcc6ae181c88f33d1b15a1
Jeremy Leo <leojeremy94@gmail.com>
2020-08-30 23:11:17 +0300

    Update dbconnection.py

--- dbconnection.py
+++ dbconnection.py
@@ -1,6 +1,6 @@
 import pymysql
 
 def connection():
-    conn = pymysql.connect(host='localhost',user='root',passwd='password',db='pythondashboard' )
+    conn = pymysql.connect(host='localhost',user='root',passwd='root08FFD8',db='sbpos' )
 
     return conn
