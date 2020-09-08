ee4ce552e23688f035ba103e98c2913a00bdde5f
Jeremy Leo <leojeremy94@gmail.com>
2020-09-08 12:22:16 +0300

    new bash script ...

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+git pull --force
+
+# Reinstall dependenceies
+source env/bin/activate
+pip3 install -r requirements.txt
+deactivate
+
+# Restart services
+systemctl status softbytedashboard  | grep 'active (running)' > /dev/null 2>&1
+
+if [ $? != 0 ]
+then
+        sudo systemctl restart softbytedashboard  > /dev/null
+fi
+
+service nginx status | grep 'active (running)' > /dev/null 2>&1
+
+if [ $? != 0 ]
+then
+        sudo service nginx restart > /dev/null
+fi
