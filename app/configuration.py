db5534893ff5d4379673d6150a9afbdfe81e6b99
Jeremy Leo <leojeremy94@gmail.com>
2020-09-10 20:36:31 +0300

    Update configuration.py

--- app/configuration.py
+++ app/configuration.py
@@ -16,4 +16,4 @@ class Config():
 	
 	SQLALCHEMY_TRACK_MODIFICATIONS 	= False
 
-	SQLALCHEMY_DATABASE_URI = 'mysql://root:rootxFFD8@localhost/sbpos'
+	SQLALCHEMY_DATABASE_URI = 'mysql://root:root08xFFD8@localhost/sbpos'
