6ebdc6b6511079c2416678f771469018d5c6b926
Jeremy Leo <leojeremy94@gmail.com>
2020-09-10 19:56:08 +0300

    Updated set customer Trans ...

--- run.py
+++ run.py
@@ -7,7 +7,7 @@ Copyright (c) 2019 - present AppSeed.us
 from app import app, db
 
 if __name__ == "__main__":
-    app.config['ENV'] = 'development'
-    app.config['DEBUG'] = True
-    app.config['TESTING'] = True
+    # app.config['ENV'] = 'development'
+    # app.config['DEBUG'] = True
+    # app.config['TESTING'] = True
     app.run()
