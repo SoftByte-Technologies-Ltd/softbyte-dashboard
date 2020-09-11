486ba859199f526abb142bb30b07a51ac0446c6b
Jeremy Leo <leojeremy94@gmail.com>
2020-09-11 12:46:02 +0300

    merge issues ...

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-#!/bin/bash
-# git pull --force
-
-# Reinstall dependenceies
-# source env/bin/activate
-# pip3 install -r requirements.txt
-# deactivate
-=======
 #!/bin/sh
 set -e
 
@@ -22,7 +13,6 @@ source env/bin/activate
 pip3 install -r requirements.txt
 deactivate
 echo "Install dependencies!"
->>>>>>> release/1.2.1
 
 # Restart services
 systemctl status softbytedashboard  | grep 'active (running)' > /dev/null 2>&1
@@ -38,11 +28,7 @@ service nginx status | grep 'active (running)' > /dev/null 2>&1
 if [ $? != 0 ]
 then
         sudo service nginx restart > /dev/null
-<<<<<<< HEAD
-fi
-=======
         echo "Nginx restarted!"
 fi
 
 echo "Application deployed!"
->>>>>>> release/1.2.1
