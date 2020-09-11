b137c6064d088eba73967d60245c113fdaa00ce7
Jeremy Leo <leojeremy94@gmail.com>
2020-09-11 12:39:59 +0300

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,18 @@
-#!/bin/bash
-git pull --force
+#!/bin/sh
+set -e
 
-# Reinstall dependenceies
+echo "Deploying application ..."
+
+# Update codebase
+git fetch origin master
+git reset --hard origin/master
+echo "Codebase updated!"
+
+# Install dependenceies
 source env/bin/activate
 pip3 install -r requirements.txt
 deactivate
+echo "Install dependencies!"
 
 # Restart services
 systemctl status softbytedashboard  | grep 'active (running)' > /dev/null 2>&1
@@ -12,6 +20,7 @@ systemctl status softbytedashboard  | grep 'active (running)' > /dev/null 2>&1
 if [ $? != 0 ]
 then
         sudo systemctl restart softbytedashboard  > /dev/null
+        echo "Python service restarted!"
 fi
 
 service nginx status | grep 'active (running)' > /dev/null 2>&1
@@ -19,4 +28,7 @@ service nginx status | grep 'active (running)' > /dev/null 2>&1
 if [ $? != 0 ]
 then
         sudo service nginx restart > /dev/null
+        echo "Nginx restarted!"
 fi
+
+echo "Application deployed!"
