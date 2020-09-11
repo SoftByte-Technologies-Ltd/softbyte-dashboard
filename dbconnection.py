7acc149a567fa3daf0ae9e3838a2793709496ab8
Jeremy Leo <leojeremy94@gmail.com>
2020-09-11 16:43:11 +0300

    Update dbconnection.py

--- dbconnection.py
+++ dbconnection.py
@@ -1,4 +1,5 @@
 import pymysql
+import os
 
 def connection():
     conn = pymysql.connect(host=os.getenv('DB_HOST'), user=os.getenv('DB_USERNAME'), passwd=os.getenv('DB_PASSWORD'),db=os.getenv('DB_DATABASE'))
